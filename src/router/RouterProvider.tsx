f18d385d7d29ad6ffa315dbe831bd3e00b0db377
tmt132 <59242928+tmt132@users.noreply.github.com>
2025-04-29 19:23:45 +0900

    feat: 기본 스타일 추가 및 라우터 수정 (#2)
    * feat: style 추가
    
    * feat: router 적용
    
    ---------
    
    Co-authored-by: unknown <tmdgml11342@gmail.com>

--- src/router/RouterProvider.tsx
+++ src/router/RouterProvider.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import Main from '@/pages/Main';
@@ -6,18 +6,32 @@ import QrPayment from '@/pages/QrPayment';
 import UsageHistory from '@/pages/UsageHistory';
 
 const CustomRouterProvider = () => {
-  const browserRouter = (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/qr" element={<QrPayment />} />
-        <Route path="/history" element={<UsageHistory />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const browserRouter = createBrowserRouter([
+    {
+      path: '',
+      element: <Main />,
+      children: [
+        {
+          path: 'qr',
+          element: <QrPayment />,
+        },
+        {
+          path: 'history',
+          element: <UsageHistory />,
+        },
+      ],
+    },
+    {
+      path: 'login',
+      element: <Login />,
+    },
+    {
+      path: 'register',
+      element: <Register />,
+    },
+  ]);
 
   return <RouterProvider router={browserRouter} future={{ v7_startTransition: true }} />;
 };
+
+export default CustomRouterProvider;
