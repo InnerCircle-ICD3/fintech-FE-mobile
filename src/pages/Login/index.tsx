2ac54d467ccb1707e28b881d2a5f57e924d19ab4
tmt132 <59242928+tmt132@users.noreply.github.com>
2025-05-17 14:59:17 +0900

    feat: 로그인 및 회원가입 페이지 구현 (#6)
    * feat: 로그인 및 회원가입 페이지 구현
    
    * feat: 임시 auth 로직 적용
    
    * feat: center 정렬 추가
    
    ---------
    
    Co-authored-by: unknown <tmdgml11342@gmail.com>

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,53 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import * as styles from '@/styles/Login.css';
+import { auth } from '@/api/auth';
+
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const success = await auth.login(email, password);
+
+    if (success) {
+      alert('login 성공!');
+    } else {
+      alert('login 실패!');
+    }
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>로그인</h1>
+      <form className={styles.form} onSubmit={handleLogin}>
+        <input
+          className={styles.input}
+          type="email"
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <br />
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br />
+        <button type="submit" className={styles.button}>
+          로그인
+        </button>
+      </form>
+      <button className={styles.secondaryButton} onClick={() => navigate('/signup')}>
+        회원가입
+      </button>{' '}
     </div>
   );
 };
