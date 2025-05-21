74826c0c4ff613bd55f8782093463e28567cd223
unknown <tmdgml11342@gmail.com>
2025-05-21 19:56:26 +0900

    feat: mock data 위치 변경

--- src/api/payments.ts
+++ src/api/payments.ts
@@ -1,22 +1,22 @@
 import { PaymentItemType } from '@/pages/UsageHistory';
 import { PaymentFilter } from '@/pages/UsageHistory/Filter';
 
-export const fetchPayments = async (filter: PaymentFilter): Promise<PaymentItemType[]> => {
-  const mockData: PaymentItemType[] = [
-    {
-      id: '1',
-      date: '2025-05-01',
-      store: '스타벅스',
-      amount: 4800,
-    },
-    {
-      id: '2',
-      date: '2025-05-03',
-      store: '이마트24',
-      amount: 2300,
-    },
-  ];
+export const mockData: PaymentItemType[] = [
+  {
+    id: '1',
+    date: '2025-05-01',
+    store: '스타벅스',
+    amount: 4800,
+  },
+  {
+    id: '2',
+    date: '2025-05-03',
+    store: '이마트24',
+    amount: 2300,
+  },
+];
 
+export const fetchPayments = async (filter: PaymentFilter): Promise<PaymentItemType[]> => {
   // 필터링 로직 (클라이언트 측 예시)
   return mockData
     .filter((item) => {
