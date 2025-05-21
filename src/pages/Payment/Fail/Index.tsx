2f92f21d044fc376034cae89b0e21838be8ce7ae
tmt132 <59242928+tmt132@users.noreply.github.com>
2025-05-21 21:04:53 +0900

    feat: mock data 적용 및 스타일 정리 (#11)
    * feat: mock data 위치 변경
    
    * feat: mock data 통해 결제 단계 구성
    
    * feat: style 수정
    
    ---------
    
    Co-authored-by: unknown <tmdgml11342@gmail.com>

--- src/pages/Payment/Fail/Index.tsx
+++ src/pages/Payment/Fail/Index.tsx
@@ -2,14 +2,20 @@ import * as styles from '@/styles/Payment.css';
 import { useNavigate } from 'react-router-dom';
 
 const Fail = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className={styles.resultContainer}>
-            <h1 className={styles.resultMessage}>결제에 실패하였습니다.</h1>
-            <button onClick={() => { navigate('/') }}>홈으로</button>
-        </div >
-    );
+  return (
+    <div className={styles.resultContainer}>
+      <h1 className={styles.resultMessage}>결제에 실패하였습니다.</h1>
+      <button
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        홈으로
+      </button>
+    </div>
+  );
 };
 
 export default Fail;
